eafcef5a14f35def1be596043ac65d7e39babdbe
Xiaofeng-Guo <xguo2@andrew.cmu.edu>
2022-05-05 22:40:15 -0400

    update

--- src/lqr_ctrl_v2.py
+++ src/lqr_ctrl_v2.py
@@ -2,19 +2,21 @@ import gym
 import d4rl
 import mujoco_py
 import numpy as np
-import time
 
-def set_state(env, qpos, qvel):
+import imageio
+
+
+
+def set_state(env, qpos):
 
 	state = env.sim.get_state()
 	for i in range(env.n_jnt):
 		state[i] = qpos[i]
 	for i in range(env.model.nq,env.model.nq+env.n_jnt):
-		state[i] = qvel[i-env.model.nq]
+		state[i] = 0.0
 	env.sim.set_state(state)
 	env.sim.forward()
 	env.sim.step()
-
 
 def linearize(env, Xref, Uref):
 
@@ -24,49 +26,46 @@ def linearize(env, Xref, Uref):
 	A = [np.zeros((n,n)) for k in range(0,N-1)]
 	B = [np.zeros((n,m)) for k in range(0,N-1)]
 
-	delta = 0.0001
+	delta = 0.00001
 
 	for step in range(N-1):
 
 		x = Xref[step]
 		u = Uref[step]
 
-		set_state(env,x[:9],x[9:18])
-		# env.sim.data.qpos[:9] = x[:9]
-		# # env.sim.data.qvel[:9] = np.zeros(9)
-		# env.sim.data.qvel[:9] = x[9:18]
-		# env.sim.forward()
+		set_state(env, x[:9])
 		observation, reward, done, info = env.step(u)
 
-		fxu = observation[:18]
+		fxu = observation[:7]
 
-		for i in range(18):
-			dx = np.zeros(18)
+		for i in range(7):
+			dx = np.zeros(9)
 			dx[i] += delta
 			# env.sim.data.qpos[:9] = x[:9] + dx[:9]
 			# env.sim.data.qvel[:9] = x[9:18] + dx[9:]
 			# env.sim.forward()
-			set_state(env,x[:9],x[9:18])
+			set_state(env, x[:9] + dx)
 			observation, reward, done, info = env.step(u)
 
-			fdxu = observation[:18]
-			A[step][:, i] = (fdxu - fxu) / delta
 
-		for i in range(9):
+			fdxu = observation[:7]
+			A[step][0:7, i] = (fdxu - fxu) / delta
+
+		for i in range(7):
 			du = np.zeros(9)
 			du[i] += delta
 			# env.sim.data.qpos[:9] = x[:9]
 			# # env.sim.data.qvel[:9] = np.zeros(9)
 			# env.sim.data.qvel[:9] = x[9:18]
 			# env.sim.forward()
-			set_state(env,x[:9],x[9:18])
+			set_state(env,x[:9])
 			observation, reward, done, info = env.step(u + du)
 
-			fxdu = observation[:18]
-			B[step][:, i] = (fxdu - fxu) / delta
+			fxdu = observation[:7]
+			B[step][:7, i] = (fxdu - fxu) / delta
 		print(step)
-	np.save('data/trial2/A.npy', A)
-	np.save('data/trial2/B.npy', B)
+	np.save('A.npy', A)
+	np.save('B.npy', B)
 
 	return A, B
 
@@ -91,38 +90,69 @@ def forward_sim(env,K,P,Xref,Uref):
 	import matplotlib.pyplot as plt
 	cost = 0
 	N = len(K)+1
+	N = 200
 
 	X = [np.zeros(18) for k in range(0,N)]
 	U = [np.zeros(9) for k in range(0,N-1)]
 	observation = env.reset()
-	X[0] = observation[:18]
-	print(X[0])
+	X[0] = observation[:9]
+
 	pid_k = np.concatenate((np.identity(9)*1,np.identity(9)*0.0),axis=1)
 
 	for k in range(0,N-1):
-		U[k] = Uref[k] - pid_k@(X[k]-Xref[k])
-
-		if k>=29 and k<45:
-			U[k][7] = -1
-			U[k][8] = -1
-
+		U[k] = Uref[k] - K[k][0:9,0:9]@(X[k]-Xref[k])
 		# U[k] = clamp.(U[k], -u_bnd, u_bnd)
 		observation, reward, done, info = env.step(U[k])
 		env.render()
-		time.sleep(0.1)
-		X[k+1] = observation[:18]
+		X[k+1] = observation[:9]
 		# X[k+1]  = true_dynamics_rk4(model, X[k], U[k], dt)
 	X = np.asarray(X)
 	Xref = np.asarray(Xref)
 
-	# plt.plot(X[:,0])
-	# plt.plot(Xref[:,0])
-	# plt.show()
+	plt.plot(X[:,5])
+	plt.plot(Xref[:,5])
+	plt.show()
 
 	return cost
 
 
 def forward_ref_sim(env,Xref,Uref):
+	# return cost
+	import matplotlib.pyplot as plt
+	cost = 0
+	# N = len(Xref)+1
+	N = 200
+	X = [np.zeros(18) for k in range(0,N)]
+	U = [np.zeros(9) for k in range(0,N-1)]
+	observation = env.reset()
+	X[0] = observation[:18]
+
+	pid_k = np.concatenate((np.identity(9)*1,np.identity(9)*0.01),axis=1)
+
+	for k in range(0,N-1):
+		# U[k] = Uref[k] - K[k]@(X[k]-Xref[k])
+		U[k] = Uref[k]
+		# if Xref[k][7] < 0.01:
+		# 	U[k][7] = -1
+		# if Xref[k,8] < 0.01:
+		# 	U[k][8] = -1
+		# U[k] = clamp.(U[k], -u_bnd, u_bnd)
+		observation, reward, done, info = env.step(U[k])
+		env.render()
+		X[k+1] = observation[:18]
+		# X[k+1]  = true_dynamics_rk4(model, X[k], U[k], dt)
+	X = np.asarray(X)
+	Xref = np.asarray(Xref)
+
+	plt.plot(X[:,1])
+	plt.plot(Xref[:,1])
+	plt.show()
+
+	return cost
+
+
+
+def fake_forward_ref_sim(env,Xref,Uref):
 	# return cost
 	import matplotlib.pyplot as plt
 	cost = 0
@@ -136,45 +166,66 @@ def forward_ref_sim(env,Xref,Uref):
 	pid_k = np.concatenate((np.identity(9)*1,np.identity(9)*0.01),axis=1)
 
 	for k in range(0,N-1):
-		U[k] = Uref[k] - K[k]@(X[k]-Xref[k])
-		# U[k] = clamp.(U[k], -u_bnd, u_bnd)
-		observation, reward, done, info = env.step(U[k])
+		set_state(env,Xref[k])
 		env.render()
-		X[k+1] = observation[:18]
-		# X[k+1]  = true_dynamics_rk4(model, X[k], U[k], dt)
-	X = np.asarray(X)
-	Xref = np.asarray(Xref)
+	return
 
-	# plt.plot(X[:,0])
-	# plt.plot(Xref[:,0])
-	# plt.show()
-
-	return cost
 
 
 
 if __name__ == '__main__':
 	env = gym.make('kitchen-complete-v0')
 	env.reset()
-	A = np.load('data/trial3/A.npy')
-	B = np.load('data/trial3/B.npy')
+	# A = np.load('A.npy')
+	# B = np.load('B.npy')
 	U_ref = np.load('data/trial3/Uref.npy')
 	X_ref = np.load('data/trial3/obs.npy')
 
-	q = [1]*18
+	q = [0.001,0.01,1,1,0.000000001,1,0.1]+[0]*2+[0]*9
 	Q = np.diag(q)
 	R = np.identity(9)*0.1
 
+	X_ref[20:29, 7:9] = 0.04
+	X_ref[29:45, 7:9] = 0.002
+	X_ref[45:60, 7:9] = 0.04
+	X_ref[65:75, 7:9] = 0.002
+
+	# U_ref[20:29, 7:9] = 0.04
+	U_ref[29:45, 7:9] = -1
+	# U_ref[45:60, 7:9] = 0.04
+	U_ref[65:75, 7:9] = -1
+
+
+
 	# A,B = linearize(env,X_ref,U_ref)
-	K,P = tvlqr(A,B,Q,R,Q)
-	print(K)
+	# K,P = tvlqr(A,B,Q,R,Q)
+	# print(K)
 	# import ipdb;ipdb.set_trace()
-	for i in range(100):
-		env.render()
-	forward_sim(env,K,P,X_ref,U_ref)
+
+	# forward_sim(env,K,P,X_ref,U_ref)
+	forward_ref_sim(env,X_ref,U_ref)
+	# fake_forward_ref_sim(env,X_ref,U_ref)
+
+	# video_name = 'video.mp4'
+	# frame = grabFrame(env)
+	# height, width, layers = frame.shape
+	# video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), 30.0, (width, height))
+	#
+	# # First pass - Step through an episode and capture each frame
+	# action_spec = env.action_spec()
+	# time_step = env.reset()
+	# while not time_step.last():
+	# 	action = np.random.uniform(action_spec.minimum,
+	# 							   action_spec.maximum,
+	# 							   size=action_spec.shape)
+	# 	time_step = env.step(action)
+	# 	frame = grabFrame(env)
+	# Render env output to video
 
 	while True:
 		env.render()
+		rgb = env.render(mode='rgb_array')
+		print(rgb)
 	# 	env.sim.data.qpos[:self.n_jnt] = reset_pose[:self.n_jnt].copy()
 	# 	env.sim.data.qvel[:self.n_jnt] = reset_vel[:self.n_jnt].copy()
 	# 	env.step(action)
--- src/stable_lqr_ctrl.py
+++ src/stable_lqr_ctrl.py
@@ -2,15 +2,15 @@ import gym
 import d4rl
 import mujoco_py
 import numpy as np
-import time
 
-def set_state(env, qpos, qvel):
+
+def set_state(env, qpos):
 
 	state = env.sim.get_state()
 	for i in range(env.n_jnt):
 		state[i] = qpos[i]
 	for i in range(env.model.nq,env.model.nq+env.n_jnt):
-		state[i] = qvel[i-env.model.nq]
+		state[i] = 0.0
 	env.sim.set_state(state)
 	env.sim.forward()
 	env.sim.step()
@@ -18,106 +18,93 @@ def set_state(env, qpos, qvel):
 
 def linearize(env, Xref, Uref):
 
-	N = Xref.shape[0]
 	n,m = 18, 9
 	env.reset()
-	A = [np.zeros((n,n)) for k in range(0,N-1)]
-	B = [np.zeros((n,m)) for k in range(0,N-1)]
+	A = np.zeros((n,n))
+	B = np.zeros((n,m))
+	delta = 0.001
 
-	delta = 0.0001
 
-	for step in range(N-1):
+	x = Xref
+	u = Uref
+	set_state(env,x[:9])
 
-		x = Xref[step]
-		u = Uref[step]
+	observation, reward, done, info = env.step(u)
+	fxu = observation[:7]
 
-		set_state(env,x[:9],x[9:18])
+	for i in range(7):
+		dx = np.zeros(9)
+		dx[i] += delta
+		# env.sim.data.qpos[:9] = x[:9] + dx[:9]
+		# env.sim.data.qvel[:9] = x[9:18] + dx[9:]
+		# env.sim.forward()
+		set_state(env,x[:9]+dx)
+		observation, reward, done, info = env.step(u)
+
+		fdxu = observation[:7]
+		A[0:7, i] = (fdxu - fxu) / delta
+
+	for i in range(7):
+		du = np.zeros(9)
+		du[i] += delta
 		# env.sim.data.qpos[:9] = x[:9]
 		# # env.sim.data.qvel[:9] = np.zeros(9)
 		# env.sim.data.qvel[:9] = x[9:18]
 		# env.sim.forward()
-		observation, reward, done, info = env.step(u)
+		set_state(env,x[:9])
+		observation, reward, done, info = env.step(u + du)
+		fxdu = observation[:7]
+		B[0:7, i] = (fxdu - fxu) / delta
 
-		fxu = observation[:18]
-
-		for i in range(18):
-			dx = np.zeros(18)
-			dx[i] += delta
-			# env.sim.data.qpos[:9] = x[:9] + dx[:9]
-			# env.sim.data.qvel[:9] = x[9:18] + dx[9:]
-			# env.sim.forward()
-			set_state(env,x[:9],x[9:18])
-			observation, reward, done, info = env.step(u)
-
-			fdxu = observation[:18]
-			A[step][:, i] = (fdxu - fxu) / delta
-
-		for i in range(9):
-			du = np.zeros(9)
-			du[i] += delta
-			# env.sim.data.qpos[:9] = x[:9]
-			# # env.sim.data.qvel[:9] = np.zeros(9)
-			# env.sim.data.qvel[:9] = x[9:18]
-			# env.sim.forward()
-			set_state(env,x[:9],x[9:18])
-			observation, reward, done, info = env.step(u + du)
-
-			fxdu = observation[:18]
-			B[step][:, i] = (fxdu - fxu) / delta
-		print(step)
-	np.save('data/trial2/A.npy', A)
-	np.save('data/trial2/B.npy', B)
+	np.save('data/trial1/stable_A.npy', A)
+	np.save('data/trial1/stable_B.npy', B)
 
 	return A, B
 
 
 def tvlqr(A,B,Q,R,Qf):
 
-	n,m = B[1].shape
-	N = len(A)+1
+	n,m = B.shape
+	N = 1000
 	K = [np.zeros((m,n)) for k in range(0,N-1)]
 	P = [np.zeros((n,n)) for k in range(0,N)]
 	P[-1] = Qf
 	for k in reversed(range(0,N-1)):
-		K[k] = np.linalg.pinv(R + B[k].T@P[k+1]@B[k]) @ (B[k].T@P[k+1]@A[k])
+		K[k] = np.linalg.pinv(R + B.T@P[k+1]@B) @ (B.T@P[k+1]@A)
 		# K[k] .= (R + B[k]'P[k+1]*B[k])\(B[k]'P[k+1]*A[k])
-		P[k] = Q + A[k].T@P[k+1]@A[k] - A[k].T@P[k+1]@B[k]@K[k]
-	return K,P
+		P[k] = Q + A.T@P[k+1]@A - A.T@P[k+1]@B@K[k]
+		print(np.linalg.norm(K[k]))
+	return K[1]
 
 # def cost()
 
-def forward_sim(env,K,P,Xref,Uref):
+def forward_sim(env,K,Xref,Uref):
 	# return cost
 	import matplotlib.pyplot as plt
 	cost = 0
-	N = len(K)+1
+	N = 1000
 
-	X = [np.zeros(18) for k in range(0,N)]
+	X = [np.zeros(9) for k in range(0,N)]
 	U = [np.zeros(9) for k in range(0,N-1)]
 	observation = env.reset()
-	X[0] = observation[:18]
-	print(X[0])
+	X[0] = observation[:9]
+
 	pid_k = np.concatenate((np.identity(9)*1,np.identity(9)*0.0),axis=1)
 
+	print(K.shape)
 	for k in range(0,N-1):
-		U[k] = Uref[k] - pid_k@(X[k]-Xref[k])
-
-		if k>=29 and k<45:
-			U[k][7] = -1
-			U[k][8] = -1
-
+		U[k] = Uref - K[0:9,0:9]@(X[k]-Xref)
 		# U[k] = clamp.(U[k], -u_bnd, u_bnd)
 		observation, reward, done, info = env.step(U[k])
 		env.render()
-		time.sleep(0.1)
-		X[k+1] = observation[:18]
+		X[k+1] = observation[:9]
 		# X[k+1]  = true_dynamics_rk4(model, X[k], U[k], dt)
 	X = np.asarray(X)
 	Xref = np.asarray(Xref)
 
-	# plt.plot(X[:,0])
-	# plt.plot(Xref[:,0])
-	# plt.show()
+	plt.plot(X[:,0])
+	plt.plot(Xref[:,0])
+	plt.show()
 
 	return cost
 
@@ -145,9 +132,9 @@ def forward_ref_sim(env,Xref,Uref):
 	X = np.asarray(X)
 	Xref = np.asarray(Xref)
 
-	# plt.plot(X[:,0])
-	# plt.plot(Xref[:,0])
-	# plt.show()
+	plt.plot(X[:,0])
+	plt.plot(Xref[:,0])
+	plt.show()
 
 	return cost
 
@@ -156,25 +143,24 @@ def forward_ref_sim(env,Xref,Uref):
 if __name__ == '__main__':
 	env = gym.make('kitchen-complete-v0')
 	env.reset()
-	A = np.load('data/trial3/A.npy')
-	B = np.load('data/trial3/B.npy')
-	U_ref = np.load('data/trial3/Uref.npy')
-	X_ref = np.load('data/trial3/obs.npy')
+	# A = np.load('data/trial2/stable_A.npy')
+	# B = np.load('data/trial2/stable_B.npy')
+	U_ref = np.load('data/trial1/uref_rough_0-74.npy')
+	X_ref = np.load('data/trial1/obs.npy')
 
-	q = [1]*18
+	q = [0.1]*9+[0]*9
 	Q = np.diag(q)
-	R = np.identity(9)*0.1
+	R = np.identity(9)*0.0001
 
-	# A,B = linearize(env,X_ref,U_ref)
-	K,P = tvlqr(A,B,Q,R,Q)
+	A,B = linearize(env,X_ref[0,:],U_ref[0,:])
+	K= tvlqr(A,B,Q,R,Q)
 	print(K)
 	# import ipdb;ipdb.set_trace()
-	for i in range(100):
-		env.render()
-	forward_sim(env,K,P,X_ref,U_ref)
 
-	while True:
-		env.render()
+	forward_sim(env,K,X_ref[0,:],U_ref[0,:])
+
+	# while True:
+	# 	env.render()
 	# 	env.sim.data.qpos[:self.n_jnt] = reset_pose[:self.n_jnt].copy()
 	# 	env.sim.data.qvel[:self.n_jnt] = reset_vel[:self.n_jnt].copy()
 	# 	env.step(action)
